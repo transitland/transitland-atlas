50a3af530e58062f062e783ee68ae44eda25c921
Drew Dara-Abrams <drew@interline.io>
2020-10-28 21:33:43 -0700

    validate operator files against JSON Schema

--- scripts/validate-operators.sh
+++ scripts/validate-operators.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# validate JSON schema
+npx ajv-cli validate -s operator-schema.json -d "../operators/*.json"
+
 # will fail if there are duplicate Onestop IDs, or if a Onestop ID or name is not present
 jq -s . ../operators/*.json | pipenv run sqlite-utils insert operators.db operators - \
   --pk=onestop_id \
