031b99b35dfc7435497387e28b08a73802d8f790
Ian Rees <ian@ianrees.net>
2021-08-20 16:35:59 -0700

    Update transitland-lib to validate operators (#306)
    * Update transitland-lib to validate operators
    
    * Validate operator onestop_id
    
    * Fix o-dnx-radartransit feed onestop_id

--- scripts/validate-feeds.py
+++ scripts/validate-feeds.py
@@ -6,6 +6,7 @@ import sqlite3
 
 fail_the_build = False
 
+# load dmfr to database
 db_filename = 'feed-validation.db'
 os.system(f"rm -f {db_filename}")
 sync_command = f"transitland dmfr sync -dburl=sqlite3://{db_filename} ../feeds/*.dmfr.json"
@@ -19,19 +20,42 @@ for log_line in sync_log.splitlines():
 db_conn = sqlite3.connect(db_filename)
 c = db_conn.cursor()
 
+# check feed onestop_id uniqueness
 c.execute('''
   SELECT onestop_id from current_feeds
 ''')
 onestop_ids = c.fetchall()
 for row in onestop_ids:
-  if row[0].count("-") == 1:
-    # TODO if re.match(r"", row[0])
-    continue
-  elif row[0].count("-") == 2:
-    # TODO
-    continue
-  else:
-    print(f"ERROR: improperly formatted Onestop ID: {row[0]}")
+  osid = row[0] or ''
+  valid = True
+  dashcount = osid.count("-")
+  if len(osid) == 0:
+    valid = False
+  if dashcount == 0 or dashcount > 2:
+    valid = False
+  if len(osid) > 0 and osid[0] != "f":
+    valid = False
+  if not valid:
+    print(f"ERROR: improperly formatted Feed Onestop ID: {osid}")
+    fail_the_build = True
+
+# check operator onestop_id uniqueness
+c.execute('''
+  SELECT onestop_id from current_operators
+''')
+onestop_ids = c.fetchall()
+for row in onestop_ids:
+  osid = row[0] or ''
+  valid = True
+  dashcount = osid.count("-")
+  if len(osid) == 0:
+    valid = False
+  if dashcount == 0 or dashcount > 2:
+    valid = False
+  if len(osid) > 0 and osid[0] != "o":
+    valid = False
+  if not valid:
+    print(f"ERROR: improperly formatted Operator Onestop ID: {osid}")
     fail_the_build = True
 
 if fail_the_build:
