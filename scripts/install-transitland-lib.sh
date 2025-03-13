dfe8b2d1e2b8a631aac3f54fd03fbe0711f9f73e
Drew Dara-Abrams <drew@interline.io>
2025-03-13 08:42:58 -0700

    use transitland-lib v1.0.0, with change to trailing line break
    related to #1468

--- scripts/install-transitland-lib.sh
+++ scripts/install-transitland-lib.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget https://github.com/interline-io/transitland-lib/releases/download/v0.16.2/transitland-linux -O transitland
+wget https://github.com/interline-io/transitland-lib/releases/download/v1.0.0/transitland-linux -O transitland
 chmod a+rx transitland
 sudo mv transitland /usr/local/bin
