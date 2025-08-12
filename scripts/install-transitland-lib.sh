b6b0122281930903d75f52d2ef5ca705fb78c844
Drew Dara-Abrams <drew@interline.io>
2025-08-12 16:57:42 -0700

    switch license on this repo to CC-BY

--- scripts/install-transitland-lib.sh
+++ scripts/install-transitland-lib.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget https://github.com/interline-io/transitland-lib/releases/download/v1.0.0/transitland-linux -O transitland
+wget https://github.com/interline-io/transitland-lib/releases/download/v1.2.14/transitland-linux -O transitland
 chmod a+rx transitland
 sudo mv transitland /usr/local/bin
