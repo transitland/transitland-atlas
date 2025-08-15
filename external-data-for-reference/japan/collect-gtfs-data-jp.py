6a5f6d35609165345e2e95e5ae58cb69d5307d74
Drew Dara-Abrams <drew@interline.io>
2025-08-14 23:28:13 -0700

    ODPT feeds (#1704)
    * ODPT feeds (and switch all Japanese scripts to uv)
    
    * fix
    
    * fix URLs missing a protocol

--- external-data-for-reference/japan/collect-gtfs-data-jp.py
+++ external-data-for-reference/japan/collect-gtfs-data-jp.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# dependencies = [
+#   "requests>=2.31.0",
+# ]
+# ///
+
 """
 Script to collect data from the GTFS Data JP API and identify feeds to add to Transitland Atlas repo.
 
